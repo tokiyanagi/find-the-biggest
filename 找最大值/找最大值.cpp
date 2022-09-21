5cc993d80fef417777786d9410b830bb5efac424
tokiyanagi <109673296+tokiyanagi@users.noreply.github.com>
2022-09-21 14:49:48 +0800

    Add files via upload

---
+++ 找最大值/找最大值.cpp
@@ -0,0 +1,19 @@
+﻿#include<stdio.h>
+int main()
+{
+	int max(int x, int y);//函数声明
+	int a, b, c;//声明变量
+	a =1;b =2;//实际参数
+	c = max(a, b);//函数调用
+	printf("a与b的最大值是%d\n",c);
+	return 0;
+}
+int max(int x,int y)//形式参数：参数类型+参数名
+{
+	int z;
+	if (x > y)
+		z = x;
+	else
+		z = y;
+	return(z);
+}
